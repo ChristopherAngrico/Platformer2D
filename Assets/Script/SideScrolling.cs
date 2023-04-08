617fee869e86105924c871b8e8cdd83bcef2ffd4
ChristopherAngrico <cenlung82@gmail.com>
2023-04-08 09:49:59 +0700

    Update

---
+++ Assets/Script/SideScrolling.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+    public Camera mainCamera;
+    private float currentPosition;
+    public Transform target;
+    private Vector3 startPosition;
+    private void Start() {
+        startPosition = transform.position;
+        // print(startPosition);
+    }
+    private void LateUpdate() {
+        // print(startPosition);
+       currentPosition = Mathf.Max(target.position.x, transform.position.x);
+       Vector3 CameraPosition = new Vector3(currentPosition, startPosition.y, startPosition.z);
+       transform.position = CameraPosition;
+    }
+}
